08135694cbc6ae3db6efd1747aa21964950a8cd3
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-05-05 14:07:20 -0300

    inputs form

--- src/pages/Checkout/components/OrderForm/index.tsx
+++ src/pages/Checkout/components/OrderForm/index.tsx
@@ -1,5 +1,10 @@
 import { CurrencyDollar, MapPinLine } from '@phosphor-icons/react'
-import { FormInfo, FormContainer } from './styles'
+import {
+  FormInfo,
+  FormContainer,
+  InputsContainer,
+  OptionalInput,
+} from './styles'
 
 export const OrderForm = () => {
   return (
@@ -12,15 +17,21 @@ export const OrderForm = () => {
             <p>Informe o endereço onde deseja receber seu pedido</p>
           </div>
         </FormInfo>
-        <div>
+        <InputsContainer>
           <input type="text" placeholder="CEP" />
           <input type="text" placeholder="Rua" />
-          <input type="number" placeholder="Número" />
-          <input type="text" placeholder="Complemento" />
-          <input type="text" placeholder="Bairro" />
-          <input type="text" placeholder="Cidade" />
-          <input type="text" placeholder="UF" />
-        </div>
+          <div>
+            <input type="number" placeholder="Número" />
+            <OptionalInput>
+              <input type="text" placeholder="Complemento" />
+            </OptionalInput>
+          </div>
+          <div>
+            <input type="text" placeholder="Bairro" />
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" />
+          </div>
+        </InputsContainer>
       </section>
       <section>
         <FormInfo iconColor="purple">
--- src/pages/Checkout/components/OrderForm/styles.ts
+++ src/pages/Checkout/components/OrderForm/styles.ts
@@ -47,3 +47,91 @@ export const FormInfo = styled.div<FormInfoProps>`
     color: ${({ theme, iconColor }) => theme[iconColor]};
   }
 `
+
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+
+  & > div {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 0.75rem;
+  }
+
+  div:nth-of-type(1) {
+    & > input {
+      width: 12.5rem;
+    }
+  }
+
+  div:nth-of-type(2) {
+    input:nth-of-type(1) {
+      width: 12.5rem;
+    }
+
+    input:nth-of-type(2) {
+      flex: 1;
+    }
+
+    input:nth-of-type(3) {
+      width: 3.75rem;
+    }
+  }
+
+  input {
+    display: block;
+    padding: 0.75rem;
+    background-color: ${({ theme }) => theme['base-input']};
+    border-radius: 4px;
+    border: 1px solid ${({ theme }) => theme['base-button']};
+    color: ${({ theme }) => theme['base-text']};
+    font-size: 0.875rem;
+    line-height: 1.3;
+    font-weight: 400;
+
+    transition: border 0.125s ease-in-out;
+  }
+
+  input:nth-child(1) {
+    width: 12.5rem;
+  }
+
+  input::placeholder {
+    color: ${({ theme }) => theme['base-label']};
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  input:focus {
+    border: 1px solid ${({ theme }) => theme['yellow-dark']};
+  }
+`
+export const OptionalInput = styled.div`
+  position: relative;
+  flex: 1;
+  display: flex;
+
+  & > input {
+    flex: 1;
+  }
+
+  &::after {
+    content: 'djkdnço';
+    position: absolute;
+    right: 8px;
+    top: 50%;
+    font-size: 15px;
+    transform: translateY(-50%);
+    color: #ccc;
+  }
+
+  &:focus-within {
+    &::after {
+      display: none;
+    }
+  }
+`
