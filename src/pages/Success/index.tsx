a2d1ff7fa3741249538ecf0b448719c3cb89be6e
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-05-13 16:46:38 -0300

    finished success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,53 @@
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
+import { IconBackgroundColors, RoundIcon } from '../../components/RoundIcon'
+import orderCompleted from './../../assets/order-completed.svg'
+import { Grid, PageContainer } from './styles'
+
 export const Success = () => {
-  return <h1>Success</h1>
+  return (
+    <PageContainer>
+      <div>
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+      </div>
+      <Grid>
+        <div>
+          <article>
+            <RoundIcon iconBackgroundColor={IconBackgroundColors.PURPLE}>
+              <MapPin size={16} weight="fill" />
+            </RoundIcon>
+            <div>
+              <p>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </article>
+          <article>
+            <RoundIcon iconBackgroundColor={IconBackgroundColors.YELLOW}>
+              <Timer size={16} weight="fill" />
+            </RoundIcon>
+            <div>
+              <p>Previsão de entrega</p>
+              <p>
+                <strong>20 min - 30 min</strong>
+              </p>
+            </div>
+          </article>
+          <article>
+            <RoundIcon iconBackgroundColor={IconBackgroundColors.YELLOW_DARK}>
+              <CurrencyDollar size={16} />
+            </RoundIcon>
+            <div>
+              <p>Pagamento na entrega</p>
+              <p>
+                <strong>Cartão de crédito</strong>
+              </p>
+            </div>
+          </article>
+        </div>
+        <img src={orderCompleted} alt="" />
+      </Grid>
+    </PageContainer>
+  )
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,87 @@
+import styled from 'styled-components'
+
+export const PageContainer = styled.section`
+  margin: 5rem 0 2rem;
+
+  & > div > h1 {
+    color: ${({ theme }) => theme['yellow-dark']};
+    font-weight: 800;
+    font-size: 2rem;
+    line-height: 1.3;
+    margin-bottom: 0.25rem;
+  }
+
+  & > div > p {
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 1.3;
+    color: ${({ theme }) => theme['base-subtitle']};
+  }
+`
+
+export const Grid = styled.div`
+  margin-top: 2.5rem;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  gap: 6.375rem;
+
+  & > div {
+    border-radius: 6px 36px;
+    border: 1px solid transparent;
+
+    background-image: linear-gradient(
+        ${({ theme }) => theme.background},
+        ${({ theme }) => theme.background}
+      ),
+      linear-gradient(102.89deg, #dbac2c 2.61%, #8047f8 98.76%);
+    background-origin: border-box;
+    background-clip: padding-box, border-box;
+
+    width: 32.875rem;
+    padding: 2.5rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 2rem;
+  }
+
+  article {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 0.75rem;
+
+    p {
+      color: ${({ theme }) => theme['base-text']};
+      font-weight: 400;
+      font-size: 1rem;
+      line-height: 1.3;
+    }
+
+    strong {
+      font-weight: 700;
+    }
+  }
+
+  @media screen and (max-width: 64rem) {
+    & {
+      margin: 3rem 0;
+      gap: 1.5rem;
+      grid-template-columns: 1fr;
+      justify-items: center;
+    }
+  }
+
+  @media screen and (max-width: 36rem) {
+    & > div {
+      width: 85vw;
+    }
+
+    & > img {
+      display: none;
+    }
+  }
+`
